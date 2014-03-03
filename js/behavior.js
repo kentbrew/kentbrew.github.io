ac0eafb2ba178c85580e5b6ed4e0670155fedfea
Kent Brewster <kent@pinterest.com>
2014-03-02 20:10:15 -0800

    whoops again

--- js/behavior.js
+++ js/behavior.js
@@ -7,7 +7,7 @@ xhr.onreadystatechange = function() {
   if (xhr.readyState == 4) {
     var r = JSON.parse(xhr.responseText);
     if (r && r.length) {
-      for (var i = r.length - 1; i > -1; i = i + 1) {
+      for (var i = r.length - 1; i > -1; i = i - 1) {
         var li = d.createElement('LI');
         var p = d.createElement('P');
         p.innerHTML = r.body;
