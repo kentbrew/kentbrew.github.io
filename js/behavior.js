e1b16fc3e1e464737ca476993fdc935b331a8abd
Kent Brewster <kent@pinterest.com>
2014-03-02 20:09:03 -0800

    try adding some comments

---
+++ js/behavior.js
@@ -0,0 +1,20 @@
+var d = document;
+var c = d.getElementById('comments');
+
+var xhr = new XMLHttpRequest();
+xhr.open("GET", 'https://api.github.com/gists/9318174/comments', true);
+xhr.onreadystatechange = function() {
+  if (xhr.readyState == 4) {
+    var r = JSON.parse(xhr.responseText);
+    if (r && r.length) {
+      for (var i = r.length - 1; i > -1; i = i + 1) {
+        var li = d.createElement('LI');
+        var p = d.createElement('P');
+        p.innerHTML = r.body;
+        li.appendChild(li);
+        c.appendChild(li);
+      }
+    }
+  }
+};
+xhr.send();
